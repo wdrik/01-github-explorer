f0570e493d0fc05fc503864ef1b238e08e014b73
wdrik <w.drik@outlook.com>
2021-04-02 17:01:05 -0300

    chore: add fast refresh

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
+import { Counter } from './components/Conter';
 import { RepositoryList } from './components/RepositoryList';
 import './styles/global.scss';
 
 export function App() {
-  return <RepositoryList />
+  return (
+    <>
+      <RepositoryList />
+      <Counter />
+    </>
+  )
 }
