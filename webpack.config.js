9141984e59213091d433841892fb18955420afd6
wdrik <w.drik@outlook.com>
2021-04-02 09:47:33 -0300

    feat: add webpack configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
-
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: path.resolve(__dirname, 'src', 'index.jsx'),
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -10,6 +11,14 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.jsx']
   },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public')
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html')
+    })
+  ],
   module: {
     rules: [
       {
